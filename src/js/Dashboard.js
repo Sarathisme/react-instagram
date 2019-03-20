fc11fd8315fe98563fb4f9dffc460ae1ba87ec44
Sarath Sattiraju <sarath.sattiraju@gmail.com>
2019-03-20 18:09:36 +0530

    Added Plus button.

--- src/js/Dashboard.js
+++ src/js/Dashboard.js
@@ -1,10 +1,15 @@
 import React, {Component} from "react";
 import Navbar from "./Navbar";
+import {Fab} from "@material/react-fab";
+import '@material/react-fab/dist/fab.css';
 
 class Dashboard extends Component {
     render() {
         return (
-          <Navbar/>
+            <div>
+                <Navbar/>
+                <Fab icon={<span className="material-icons"><i className="fas fa-plus-circle"/></span>}/>
+            </div>
         );
     }
 }
--- src/js/Navbar.js
+++ src/js/Navbar.js
@@ -3,11 +3,20 @@ import {withCookies} from "react-cookie";
 import '../css/Navbar.css';
 
 class Navbar extends Component{
+
+    logout(response) {
+
+    }
+
+    addPost() {
+
+    }
+
     render() {
         return(
             <div className='nav-bar'>
                 <div className='container nav-bar-content'>
-                    <div className="row nav-bar-row">
+                    <div className="container row nav-bar-row">
                         <div className="nav-content-left col-lg-3 col-md-6 col-sm-6">
                             <img src={process.env.PUBLIC_URL+'/favicon.ico'} width="36" height="36" alt='logo'/>
                             <div id="divider"/>
@@ -17,7 +26,7 @@ class Navbar extends Component{
                             <input type="text" className='form-control form-control-sm' placeholder="Search"/>
                         </div>
                         <div className="col-lg-3 col-md-6 col-sm-6 nav-content-right">
-                            <i className="fas fa-sign-out-alt"/>
+                            <i className="fas fa-sign-out-alt" onClick={this.logout}/>
                         </div>
                     </div>
                 </div>
